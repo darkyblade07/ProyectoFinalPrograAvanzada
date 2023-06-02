001e4f997d1f5545e92fd89e975fffa031683148
darkyblade07 <101464523+darkyblade07@users.noreply.github.com>
2023-06-02 17:33:51 -0400

    Created Entities Project

---
+++ Entities/UserEnt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalPrograAvanzada.Entities
+{
+    public class UserEnt
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool State { get; set; }
+    }
+}
